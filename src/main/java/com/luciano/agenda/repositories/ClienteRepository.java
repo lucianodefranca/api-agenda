5fe9e1f57adab2aef9227716c9dc3e0c543cb615
luciano de franca <lucianodefranca@gmail.com>
2022-08-21 22:19:16 -0300

    Interface ClienteRepository criada

---
+++ src/main/java/com/luciano/agenda/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.luciano.agenda.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.luciano.agenda.entities.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
